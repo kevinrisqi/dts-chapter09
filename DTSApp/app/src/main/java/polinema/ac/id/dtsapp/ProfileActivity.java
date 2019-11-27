e26c4c7967acbc92ff7c3c0cf24d70d09f24378f
Kevin <kevinrisqi22@gmail.com>
2019-11-27 18:05:30 +0700

    Add show, update, delete operation in Profile Activity

--- DTSApp/app/src/main/java/polinema/ac/id/dtsapp/ProfileActivity.java
+++ DTSApp/app/src/main/java/polinema/ac/id/dtsapp/ProfileActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import polinema.ac.id.dtsapp.data.AppDbProvider;
+import polinema.ac.id.dtsapp.data.User;
+import polinema.ac.id.dtsapp.data.UserDao;
+
 
 public class ProfileActivity extends AppCompatActivity
 {
@@ -17,6 +21,8 @@ public class ProfileActivity extends AppCompatActivity
     private EditText edtEmail;
     private EditText edtPhoneNumber;
     private Button btnSave;
+
+    private User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,9 @@ public class ProfileActivity extends AppCompatActivity
 
     private void loadData()
     {
+        UserDao daoUser = AppDbProvider.getInstance(this).userDao();
 
+        this.currentUser = daoUser.selectOne();
     }
 
     private void initComponents()
@@ -39,15 +47,47 @@ public class ProfileActivity extends AppCompatActivity
         this.edtEmail = this.findViewById(R.id.edt_email);
         this.edtPhoneNumber = this.findViewById(R.id.edt_phone_number);
         this.btnSave = this.findViewById(R.id.btn_save);
+
+        if (currentUser == null){
+            this.btnSave.setEnabled(false);
+            return;
+        }
+        this.edtUsername.setText(this.currentUser.username);
+        this.edtPassword.setText(this.currentUser.password);
+        this.edtEmail.setText(this.currentUser.email);
+        this.edtPhoneNumber.setText(this.currentUser.phoneNumber);
+    }
+
+    private void syncData(){
+        this.currentUser.username = this.edtUsername.getText().toString();
+        this.currentUser.password = this.edtPassword.getText().toString();
+        this.currentUser.email = this.edtEmail.getText().toString();
+        this.currentUser.phoneNumber = this.edtPhoneNumber.getText().toString();
     }
 
     public void onBtnSave_Click(View view)
     {
+        this.syncData();
+
+        UserDao daoUser = AppDbProvider.getInstance(this).userDao();
+
+        daoUser.update(this.currentUser);
+
         Toast.makeText(this, "Your data has been updated!", Toast.LENGTH_SHORT).show();
     }
 
     public void onTxvDeleteAccount_Click(View view)
     {
+        this.syncData();
+
+        UserDao daoUser = AppDbProvider.getInstance(this).userDao();
+
+        daoUser.delete(this.currentUser);
+
         Toast.makeText(this, "Your data has been deleted..", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(getApplicationContext(),WelcomeBackActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
